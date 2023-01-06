f6dc85fb6c29e0c2b1ca942338022355f342d348
Magesh Ravi <magesh@webinative.com>
2023-01-06 16:55:54 +0530

    setup postgres
    - sets up postgres container
    - updates pip-requirements to include postgres adapter
    - configures django app to use postgres db

--- dockerise_django/settings.py
+++ dockerise_django/settings.py
@@ -99,8 +99,15 @@ WSGI_APPLICATION = "dockerise_django.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.postgresql_psycopg2",
+        "NAME": os.getenv("DJANGO_DB_NAME"),
+        "USER": os.getenv("DJANGO_DB_USER"),
+        "PASSWORD": os.getenv("DJANGO_DB_PASSWORD"),
+        "HOST": os.getenv("DJANGO_DB_HOST"),
+        "PORT": os.getenv("DJANGO_DB_PORT"),
+        "TEST": {
+            "NAME": "testDB_dockerise_django",
+        },
     }
 }
 
